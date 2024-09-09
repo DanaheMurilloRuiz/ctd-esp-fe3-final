222735bfdb39a1eabf988ac4ce4dbf1f3278d0a0
DanaheMurilloRuiz <danahe.murillo.ruiz@gmail.com>
2024-09-09 10:59:48 -0500

    dependencias listas y routes listas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from './Components/Footer';
+import Navbar from './Components/Navbar';
 
-import Footer from "./Components/Footer";
-import Navbar from "./Components/Navbar";
-
+import Home from './Routes/Home'
+import Contact from './Routes/Contact';
+import Detail from './Routes/Detail';
+import Favs from './Routes/Favs';
 
 function App() {
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+    <div className="App">
+      <Router>
+        <Navbar />
+
+        <Routes>
+           <Route path="/home" element={<Home />} />
+          <Route path="/contacto" element={<Contact />} />
+          <Route path="/dentista/:id" element={<Detail />} />
+          <Route path="/favs" element={<Favs />} />
+        </Routes>
+
+        <Footer /> 
+      </Router>
+    </div>
   );
 }
 
